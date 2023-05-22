186e1bfe548df717c660e6000a23503a39746d71
pprzemek56 <przemkopis@wp.pl>
2023-05-23 01:56:50 +0200

    implemnt remove_url, remove_at, remove_hashtag methods for manipulate dataset and implment analyze_tweets for creating the statistic csv file with sentimnet of tweets from dataset

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import string
 import nltk
+import re
+import pandas as pd
 
+from collections import defaultdict
 from nltk.corpus import stopwords
 from nltk.sentiment import SentimentIntensityAnalyzer
 
@@ -10,17 +13,64 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 
 
 def main():
+    analyze_tweets()
+
+
+def analyze_tweets():
     stop_words = set(stopwords.words('english'))
     sentiment_analyzer = SentimentIntensityAnalyzer()
+    df = pd.read_csv('tweets_after.csv')
 
+    results_dict = defaultdict(lambda: defaultdict(int))
+    for i, row in df.iterrows():
+        name = row['handle']
+        tweet = row['text']
+
+        # manipulate the dataset
+        tweet = remove_hashtag(tweet)
+        tweet = remove_at(tweet)
+        tweet = remove_url(tweet)
+        tweet = remove_punctuation(tweet)
+        tokens = tokenize(tweet)
+        tokens = remove_stopwords(tokens, stop_words)
+
+        # analyze the sentiment
+        result = sentiment(' '.join(tokens), sentiment_analyzer)['compound']
+
+        if result < -0.1:
+            result = 'negative'
+        elif result > 0.1:
+            result = 'positive'
+        else:
+            result = 'neutral'
+
+        results_dict[name][result] += 1
+
+    df2 = pd.DataFrame([{'name': 'Donald Trump' if n == 'realDonaldTrump' else 'Hillary Clinton',
+                         'sentiment': r, 'count': c}
+                        for n, r in results_dict.items()
+                        for r, c in r.items()])
+
+    df2.to_csv('tweet_sentiments.csv', index=False)
 
 
 def sentiment(text, sentiment_analyzer):
     return sentiment_analyzer.polarity_scores(text)
 
 
-def remove_stopwords(tokens, stop_words):
+def remove_hashtag(text):
+    return re.sub(r'#\S+', '', text)
 
+
+def remove_at(text):
+    return re.sub(r'@\S+', '', text)
+
+
+def remove_url(text):
+    return re.sub(r'http\S+|www.\S+', '', text)
+
+
+def remove_stopwords(tokens, stop_words):
     filtered_tokens = [token for token in tokens if token not in stop_words]
     return filtered_tokens
 
