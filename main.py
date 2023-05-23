6b0751b4985ed0a5641ab420d683eff75023788f
pprzemek56 <przemkopis@wp.pl>
2023-05-23 21:57:21 +0200

    implement graph_visualization, function for creating the graph of sentiment

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import string
 import nltk
 import re
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from collections import defaultdict
 from nltk.corpus import stopwords
@@ -13,7 +14,54 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 
 
 def main():
-    analyze_tweets()
+    graph_visualization()
+
+
+def graph_visualization():
+    df = pd.read_csv('tweet_sentiments.csv')
+    clinton_df = df[df['name'] == 'Hillary Clinton']
+    trump_df = df[df['name'] == 'Donald Trump']
+    clinton_dict = clinton_df.set_index('sentiment')['count'].to_dict()
+    trump_dict = trump_df.set_index('sentiment')['count'].to_dict()
+
+    groups = list(clinton_dict.keys())
+
+    clinton_values = list(clinton_dict.values())
+    trump_values = list(trump_dict.values())
+
+    num_groups = len(groups)
+
+    bar_width = 0.4
+
+    fig, ax = plt.subplots()
+
+    clinton_positions = [i for i in range(num_groups)]
+    trump_positions = [i + bar_width for i in range(num_groups)]
+
+    clinton_bars = ax.bar(clinton_positions, clinton_values, bar_width, label='Clinton')
+    trump_bars = ax.bar(trump_positions, trump_values, bar_width, label='Trump')
+
+    def add_counts(bars):
+        for bar in bars:
+            height = bar.get_height()
+            ax.text(bar.get_x() + bar.get_width() / 2, height,
+                    '%d' % int(height),
+                    ha='center', va='bottom')
+
+    # Annotate counts below bars
+    add_counts(clinton_bars)
+    add_counts(trump_bars)
+
+    ax.set_xlabel('Sentiment')
+    ax.set_ylabel('Count')
+    ax.set_title("Sentiment analysis of Donald Trump's and Hillary Clinton's tweets")
+    ax.set_xticks([r + bar_width / 2 for r in range(num_groups)])
+    ax.set_xticklabels(groups)
+    ax.legend()
+
+    plt.show()
+
+
 
 
 def analyze_tweets():
